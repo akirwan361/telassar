299af1b28c4f37c1e1b719ac0a1425a5f459b86f
almsivi <andrew.miller@mu.ie>
2020-01-09 13:55:13 +0000

    check it

--- main.py
+++ main.py
@@ -1,23 +1,49 @@
 import numpy as np
 from astropy.io import fits
 from telassar import PVSlice
+from telassar.tools import is_notebook
 import astropy.units as u
 import matplotlib.pyplot as plt
 
-def header():
-    """Usage function"""
-    print("\nA script to load two data types for testing the `telassar` module:")
-    print("one file is already in PV-format; the other is a cube which this will")
-    print("automatically reduce to 2D. Both show a [SII] line from a young stellar")
-    print("jet taken with the MUSE and X-Shooter instruments, respectively.")
-    print("\nFurther, this will show how a `PVSlice` object can be loaded from")
-    print("either a numpy data array (provided a header is passed), or from a")
-    print("FITS file directly (provided it is already in PV-format).")
-    print("")
 
-header()
-#def main():
-"""Load the files from the directory"""
+"""
+This script loads two data cases for testing the `telassar` module: one file is
+already in PV-format; the other file is a data cube which will automatically be
+reduced to 2D for simplicity. Both show [SII] emissions from a young stellar jet
+taken with the MUSE and X-Shooter instruments, respectively.
+
+Further, this will show how a `PVSlice` object can be loaded from
+either a numpy data array (provided a header is passed), or from a
+FITS file directly (provided it is already in PV-format).
+
+Basic usage:
+
+If loading from a FITS file,
+
+    from telassar import PVSlice
+    obj = PVSlice(filename = fname)
+
+Info about the object can be accessed by `obj.info()`
+
+If loading from a numpy array,
+
+    obj = PVSlice(data = my_data)
+
+The `obj.info()` call will attempt to print both data info and world coordinates
+info, so if a data array is passed with no `header` argument, no coordinates will
+be installed. Otherwise, pass a FITS-format header with `header = my_header`.
+
+Running the script will ask if you want to see the printed info, and if you want
+to see the position-velocity plots of the data.
+
+Other attributes you can check:
+    obj.position -- view the spatial WCS info
+    obj.velwave -- view the spectral/velocity WCS info
+
+"""
+
+
+# Load the files from the directory
 
 fname1 = "SII_rotated_new.fits"
 fname2 = "data/HD163296_SIIF_6730.fits"
@@ -32,11 +58,16 @@ muse_sii = PVSlice(filename = fname1, data = muse_pv, header = hdr1)
 # load from a FITS file
 xshoo_sii = PVSlice(fname2)
 
-print("Printing info for `muse_sii`: \n")
-muse_sii.info()
-print("\nPrinting info for `xshoo_sii`: \n")
-xshoo_sii.info()
-print("")
+show_data = input("Print data info? [y/n] ")
+
+if show_data.lower() == 'y':
+    print("\nPrinting info for `muse_sii`: \n")
+    muse_sii.info()
+    print("\nPrinting info for `xshoo_sii`: \n")
+    xshoo_sii.info()
+    print("")
+else:
+    pass
 
 show_plot = input("Show contour plots? [y/n] ")
 
@@ -45,6 +76,10 @@ if show_plot.lower() == 'y':
     #ax2 = show_contours(xshoo_sii)
     ax1 = muse_sii.plot_contours(emline = 'SII6731')
     ax2 = xshoo_sii.plot_contours(emline = 'SII6731')
-    plt.show()
+
+    if is_notebook():
+        plt.ion()
+    else:
+        plt.show()
 else:
     pass
