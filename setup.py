bc0c78ccbe51a641567339aba4517a47afe786d9
amiller <andrew.miller@mu.ie>
2021-04-09 14:41:33 +0100

    refactors

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 setup(
     name='telassar',  # Required
     version='0.0.1',  # Required
-    description='Two dimEnsionaL spectrAl analysiS for muSe And xshooteR,'  # Optional
+    description='Two dimEnsionaL spectrAl analysiS for muSe And xshooteR',  # Optional
     long_description=long_description,  # Optional
     long_description_content_type='text/markdown',  # Optional (see note above)
     url='https://github.com/amiller361/telassar',  # Optional
@@ -32,17 +32,16 @@ setup(
         'lmfit',
         'photutils',
     ],  # Optional
-    
+
     # include sample data?
-    data_files=[('dgtau_OI6300_pvslice.fits' : ['data/dgtau_OI6300_pvslice.fits'])]  
-        '
+    data_files=[('dgtau_OI6300_pvslice.fits', ['data/dgtau_OI6300_pvslice.fits'])],        
 #    entry_points={  # Optional
 #        'console_scripts': [
 #            'sample=sample:main',
 #        ],
 #    },
 
-        project_urls={  # Optional
+     project_urls={  # Optional
         'Bug Reports': 'https://github.com/amiller361/telassar/issues',
 #        'Say Thanks!': 'http://saythanks.io/to/example',
         'Source': 'https://github.com/amiller361/telassar/',
